879976d839ea4b187956ecded746c32f62bfeedc
Nikolay Golovin <nik.golovin83@gmail.com>
2022-02-13 16:57:55 +0600

    улучшил interface в types

--- project/src/types/types.tsx
+++ project/src/types/types.tsx
@@ -1,36 +1,40 @@
+export interface ILocation {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+}
+
+export interface IHotelsHost {
+  avatarUrl: string;
+  id: number;
+  isPro: boolean;
+  name: string;
+}
+
 export interface ICityLocation {
-    latitude: number;
-    longitude: number;
-    zoom: number;
+  latitude: number;
+  longitude: number;
+  zoom: number;
 }
 
 export interface IHotels {
-  bedrooms: number
-city: {
-location: ICityLocation;
-name: string
-}
-description: string
-goods: [string]
-host: {
-avatarUrl: string
-id: number
-isPro: boolean
-name: string
-}
-id: number
-images: [string]
-isFavorite: boolean
-isPremium: boolean
-location: {
-latitude: number
-longitude: number
-zoom: number
-}
-maxAdults: number
-previewImage: string
-price: number
-rating: number
-title: string
-type: string
+  bedrooms: number;
+  city: {
+    location: ICityLocation;
+    name: string;
+  }
+  description: string;
+  goods: string[];
+  host: IHotelsHost;
+  id: number;
+  images: string[];
+  isFavorite: boolean;
+  isPremium: boolean;
+  location: ILocation;
+  maxAdults: number;
+  previewImage: string;
+  price: number;
+  rating: number;
+  title: string;
+  type: string;
 }
