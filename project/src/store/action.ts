4e962bfc23db3052f7b304f88e7801b31bc7a7ce
Nik G <nik.golovin83@gmail.com>
2022-07-05 22:21:29 +0600

    action и reducer для авторизации

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,5 +1,7 @@
 import { createAction } from '@reduxjs/toolkit';
+import { AuthorizationStatus } from '../const';
 
 export const loadData = createAction<[]>('load-data');
 export const changeCity = createAction<string>('change-city');
 export const offerList = createAction('offer-list');
+export const changeAuthStatus = createAction<AuthorizationStatus>('change-auth-status');
--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,16 +1,19 @@
 import { IHotels } from './../types/types';
 import { createReducer } from '@reduxjs/toolkit';
-import { changeCity, loadData } from './action';
+import { changeCity, loadData, changeAuthStatus } from './action';
+import { AuthorizationStatus } from '../const';
 
 
 interface HotelsState {
   offerList: IHotels[];
   city: string;
+  authorizationStatus: string;
 }
 
 const initialState: HotelsState = {
   offerList: [],
   city: 'Paris',
+  authorizationStatus: AuthorizationStatus.Unknown,
 };
 
 export const reducer = createReducer (initialState, (builder) => {
@@ -20,6 +23,9 @@ export const reducer = createReducer (initialState, (builder) => {
     })
     .addCase(changeCity, (state, action) => {
       state.city = action.payload;
+    })
+    .addCase(changeAuthStatus, (state, action) => {
+      state.authorizationStatus = action.payload;
     });
 });
 
